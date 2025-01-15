70983698bacc0c60b145049a43791eb47a14f8c0
jcbantuelle <jcbantuelle@gmail.com>
2025-01-15 15:43:38 -0600

    Lamulana: Rewrite Shop Item Name Text

--- worlds/lamulana/DatMod.py
+++ worlds/lamulana/DatMod.py
@@ -3,6 +3,39 @@ from .Dat import Dat
 from .Items import item_table
 from .LmFlags import GLOBAL_FLAGS, HEADERS, CARDS
 
+FONT = \
+        u"!\"&'(),-./0123456789:?ABCDEFGHIJKLMNOPQRSTUVWXYZ"\
+        u"　]^_abcdefghijklmnopqrstuvwxyz…♪、。々「」ぁあぃいぅうぇえぉおか"\
+        u"がきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほ"\
+        u"ぼぽまみむめもゃやゅゆょよらりるれろわをんァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセ"\
+        u"ゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリル"\
+        u"レロワヲンヴ・ー一三上下不与世丘両中丸主乗乙乱乳予争事二人今介仕他付代以仮仲件会伝位低住体何作使"\
+        u"供侵係保信俺倍倒値偉側偶備傷像僧元兄先光兜入全公具典内再冒冥出刀分切列初別利刻則前剣創力加助効勇"\
+        u"勉動化匹十半協博印危去参双反取受叡口古召可台史右司合同名向否周呪味呼命品唯唱問喜営器噴四回囲図国"\
+        u"土在地坂型域基堂報場塊塔墓増壁壇壊士声売壷変外多夜夢大天太央失奇契奥女好妊妖妻始姿娘婦子字存孤学"\
+        u"宇守官宙定宝実客室宮家密寝対封専導小少尾屋屏属山岩崖崩嵐左巨己布帯帰常年幸幻幾広床底店度座庫廊廟"\
+        u"弁引弟弱張強弾当形影役彼待後心必忍忘応念怒思急性怨恐息恵悔悟悪悲情惑想意愚愛感慈態憶我戦戻所扉手"\
+        u"扱投抜押拝拡拳拾持指振探撃撮操支攻放敗教散数敵敷文料斧断新方旅族日早昇明昔星映時晩普晶智暗曲書最"\
+        u"月有服望未末本杉村杖束来杯板析果架柱査格械棺検椿楼楽槍様槽模樹橋機欠次欲歓止正武歩歯歳歴死殊残段"\
+        u"殺殿母毒毛気水氷永求汝池決治法波泥注洞洪流海消涙涯深済減湖満源溶滅滝火灯灼炎無然熱爆爪父版牛物特"\
+        u"犬状狂独獄獅獣玄玉王珠現球理瓶生産用男画界略番発登白百的盤目直盾看真眠着知石研破碑示礼社祈祖神祠"\
+        u"祭禁福私秘秤移種穴究空突窟立竜章竪端笛符第筒答箱範精系約納純紫細紹終経結続緑練罠罪罰義羽習翻翼老"\
+        u"考者耐聖聞肉肩胸能脱腕自至船色若苦英荷華落葉蔵薇薔薬蛇血行術衛表裁装裏補製複要見覚親解言記訳証試"\
+        u"話詳認誕誘語誠説読誰調論謁謎謝識議護谷貝財貧貯買貸資賢贄贖赤走起超足跡路踊蹴身車軽輝辞込辿近返迷"\
+        u"追送逃通速造連進遊過道達違遠適選遺還郎部配重野量金針鉄銀銃銅録鍵鎖鏡長門閉開間関闇闘防限険陽階隠"\
+        u"雄雑難雨霊青静面革靴音順領頭題顔願類風飛食館馬駄験骨高魂魔魚鳥鳴黄黒泉居転清成仏拠維視宿浮熟飾冷"\
+        u"得集安割栄偽屍伸巻緒捨固届叩越激彫蘇狭浅Ⅱ［］：！？～／０１２３４５６７８９ＡＢＣＤＥＦＧＨＩＪ"\
+        u"ＫＬＭＮＯＰＲＳＴＵＶＷＸＹａｂｄｅｇｈｉｌｍｏｐｒｓｔｕｘ辺薄島異温復称狙豊穣虫絶ＱＺｃｆｊｋ"\
+        u"ｎｑｖｗｙｚ＋－旧了設更横幅似確置整＞％香ü描園為渡象相聴比較掘酷艇原民雷絵南米平木秋田県湯環砂"\
+        u"漠角運湿円背負構授輪圏隙草植快埋寺院妙該式判（）警告収首腰芸酒美組各演点勝観編丈夫姫救’，．霧節"\
+        u"幽技師柄期瞬電購任販Á;û+→↓←↑⓪①②③④⑤⑥⑦⑧⑨<”挑朝痛魅鍛戒飲憂照磨射互降沈醜触煮疲"\
+        u"素競際易堅豪屈潔削除替Ü♡*$街極ＵＤＦ▲✖■●✕七並久五亜亡交仰余依便修個借倣働儀償優免六共冑"\
+        u"冠冶凄凍凶刃制刺労勢勿包医卑単厄及吐含吸吹咆和員哮哺商善喰噂噛嚇因団困圧垂執塗塞境奪威婆嫌完害容"\
+        u"寄寒寛察尋尽峙巡巧差幼建弄彩往徊従徘御微徳徴忌怖怪恨悠慢慮憑憧扇才払抱担拶拷挙挨捕排掛掟接揃揮故"\
+        u"敏敢旋既旺昂昆春是暑暮暴朽材枚枝染柔株根案棒森業権歌油泳活派浴液測準潜烈烏焼燃爵片牽狩狼猛猟猫献"\
+        u"猿獲率珍甦由甲病症痩療癒皮益盛監眼睡矛短砕硬磁礁禽秀程穏筋管築簡粉粘糞級給統継綿総線縁縛縦織羅羊"\
+        u"群耳職肌股肢肪育脂脅脈脚腐膚膜臭致興舞般良花荒葬蛮被裂襲覆討託訪詰諸貢質赦趣距跳軍軟迂迎迫逆透途"\
+        u"這遅遥避邪都酸銭鋭錬鎌鑑闊阻陥陰陸障離震露非預頼額養騙驚骸髪鱗鶏鹿鼻龍"
 
 class DatMod(FileMod):
 
@@ -17,6 +50,7 @@ class DatMod(FileMod):
         }
         super().set_params(params)
         for card_index in location.cards:
+            params["card"] = self.file_contents.cards[card_index]
             params["entries"] = self.file_contents.cards[card_index].contents.entries
 
             if location.slot is None:
@@ -41,7 +75,7 @@ class DatMod(FileMod):
 
     # DAT Mod Methods
 
-    def __place_conversation_item(self, entries, item_id, location, item, original_obtain_flag, new_obtain_flag, obtain_value):
+    def __place_conversation_item(self, card, entries, item_id, location, item, original_obtain_flag, new_obtain_flag, obtain_value):
         item_index = next((i for i, v in enumerate(entries) if v.header == HEADERS["item"] and v.contents.value == location.item_id), None)
         entries[item_index].contents.value = item_id
 
@@ -49,7 +83,7 @@ class DatMod(FileMod):
         entries[flag_index].contents.address = new_obtain_flag
         entries[flag_index].contents.value = obtain_value
 
-    def __place_shop_item(self, entries, item_id, location, item, original_obtain_flag, new_obtain_flag, obtain_value):
+    def __place_shop_item(self, card, entries, item_id, location, item, original_obtain_flag, new_obtain_flag, obtain_value):
         # Override Other Player Item to Map if in a Shop to prevent quantity from selling out
         if item_id == item_table["Holy Grail (Full)"].game_code:
             item_id = item_table["Map (Surface)"].game_code
@@ -62,6 +96,32 @@ class DatMod(FileMod):
         entries[data_indices[3]].contents.values[location.slot] = new_obtain_flag
         if obtain_value > 1:
             entries[data_indices[6]].contents.values[location.slot] = new_obtain_flag
+
+        # Set New Item Name In Shop Description
+
+        # The item descriptions in a shop are always the 7th, 8th, and 9th lines, so we want to start from the 6th break
+        break_indices = [i for i, v in enumerate(entries) if v.header == HEADERS["break"]]
+        item_description_start_index = break_indices[6+location.slot]
+
+        # The item name always appears between color entries
+        color_indices = [i+item_description_start_index for i, v in enumerate(entries[item_description_start_index:]) if v.header == HEADERS["color"]]
+        item_name_start_index = color_indices[0] + 1
+
+        # Encode the new item name as Entries
+        item_name = location.item.name if location.item and location.item.name is not None else "Unknown"
+        item_name_entries = [self.__char_entry(codepoint) for codepoint in self.__encode(item_name)]
+
+        # Remove the old item name
+        del entries[item_name_start_index:color_indices[1]]
+        removed_name_size = 2 * (color_indices[1] - color_indices[0] - 1)
+        card.len_contents -= removed_name_size
+        self.file_size -= removed_name_size
+
+        # Add thew new item name
+        entries[item_name_start_index:item_name_start_index] = item_name_entries
+        added_name_size = 2 * len(item_name_entries)
+        card.len_contents += added_name_size
+        self.file_size += added_name_size
 
     def __rewrite_xelpud_flag_checks(self) -> None:
         card = self.__find_card("xelpud_conversation_tree")
@@ -209,3 +269,27 @@ class DatMod(FileMod):
             del entries[index]
             self.file_size -= 6
             card.len_contents -= 6
+
+    def __decode(self, entries):
+        text = [FONT[entry.header-0x100] for entry in entries]
+        return ''.join(text)
+
+    def __encode(self, text):
+        return [self.__encode_char(char) for char in text]
+
+    def __encode_char(self, char):
+        if char == " ":
+            return HEADERS["white_space"]
+        encoded_char = FONT.find(char)
+        if encoded_char == -1:
+            print("Busted character: ", char)
+            encoded_char = FONT.find("?")
+            print("Fixed character: ", encoded_char)
+        return encoded_char + 0x100
+
+    def __char_entry(self, codepoint):
+        char_entry = Dat.Entry()
+        char_entry.header = codepoint
+        char_entry.contents = Dat.Noop()
+        char_entry.contents.no_value = bytearray()
+        return char_entry
